489ccdebc15da85f4bf8501adad9dfd5cf8b1e26
Jayansh Devgan <72650671+JayanshDevgan@users.noreply.github.com>
2025-03-26 01:26:32 +0530

    GLEW is now working fine

--- main.cpp
+++ main.cpp
@@ -1,45 +1,40 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
-#include <GL/glew.h>
 #include <iostream>
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
 
 int main(void)
 {
-    glfwInit();
-    glewInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow* window;
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", NULL, NULL);
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
-        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
 
+    /* Make the window's context current */
     glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f,
-    };
 
-    unsigned int VBO, VAO;
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error!" << std::endl;
 
+    /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        /* Swap front and back buffers */
         glfwSwapBuffers(window);
+
+        /* Poll for and process events */
         glfwPollEvents();
     }
 
